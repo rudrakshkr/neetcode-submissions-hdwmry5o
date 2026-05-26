4e3a77bf4fcf1a627b55c0d76e15eb0c7d8961a5
Rudraksh <84639731+rudrakshkr@users.noreply.github.com>
2026-05-26 18:28:43 +0530

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canJump(nums) {
+        let farthest = 0;
+        for(let i = 0; i < nums.length; i++) {
+            if(i > farthest) {
+                return false;
+            }
+            
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if(farthest >= nums.length - 1) return true;
+        }
+    }
+}
