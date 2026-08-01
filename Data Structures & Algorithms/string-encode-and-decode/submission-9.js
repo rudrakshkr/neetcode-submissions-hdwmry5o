42b41e35bcd8295fdc961e2d2f6b0326432931a0
Rudraksh <84639731+rudrakshkr@users.noreply.github.com>
2026-08-01 13:50:29 +0530

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encodedString = "";
+        for(let i = 0; i < strs.length; i++) {
+            let strLength = strs[i].length;
+            encodedString += `${strLength}#${strs[i]}`
+        }
+
+        console.log(encodedString);
+        return encodedString;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let decodedList = [];
+        let i = 0;
+
+        while(i !== str.length) {
+            // Find # and everything before it is the length
+            let j = i;
+            while(str[i] !== "#") {
+                i += 1
+            }
+            let length = Number(str.slice(j, i));
+
+            i += 1;
+
+            decodedList.push(str.substring(i, i + length));
+            i += length;
+        }  
+
+        return decodedList;
+    }
+}
