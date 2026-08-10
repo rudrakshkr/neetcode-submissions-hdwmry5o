024a272588e1e486815cf0f7f913fb5995021a25
Rudraksh <84639731+rudrakshkr@users.noreply.github.com>
2026-08-10 19:20:21 +0530

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let cleanedStr = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+        let i = 0;
+        let j = cleanedStr.length - 1;
+
+        while(i <= j) {
+            
+            if(cleanedStr[i] !== cleanedStr[j]) return false;
+            i++
+            j--
+        }
+
+        return true;
+    }
+}
