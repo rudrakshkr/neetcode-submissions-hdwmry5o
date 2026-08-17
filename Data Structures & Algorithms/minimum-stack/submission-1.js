f3b0fcbf7cc8aa171776cdbe70f2fe0ac2acdab3
Rudraksh <84639731+rudrakshkr@users.noreply.github.com>
2026-08-17 18:05:11 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.js
@@ -0,0 +1,51 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        if(this.stack.length === 0) {
+            this.stack.push(
+                {
+                    val: val,
+                    min: val
+                }
+            );
+        }
+        else {
+            let getMin = this.stack.at(-1).min;
+
+            let newMin = Math.min(getMin, val);
+
+            this.stack.push({
+                val: val,
+                min: newMin
+            })
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack.at(-1).val;
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.stack.at(-1).min;
+    }
+}
