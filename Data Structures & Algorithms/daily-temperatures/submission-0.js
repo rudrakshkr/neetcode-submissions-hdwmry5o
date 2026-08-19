869d0caaa20e98a88bf25a8fde1d69d022ce25ec
Rudraksh <84639731+rudrakshkr@users.noreply.github.com>
2026-08-19 12:59:11 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let answer = new Array(temperatures.length).fill(0);
+        let stack = [0];
+
+        for(let i = 1; i < temperatures.length; i++) {
+            while( stack.length > 0 
+                && temperatures[i] > temperatures[stack.at(-1)] 
+            ) {
+                let prev = stack.pop();
+                answer[prev] = i - prev;
+            }
+
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
