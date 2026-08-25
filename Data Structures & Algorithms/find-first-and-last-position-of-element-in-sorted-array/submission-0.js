c3412a0d8cffe315344fb51e37ef77cd142a24b0
Rudraksh <84639731+rudrakshkr@users.noreply.github.com>
2026-08-25 18:14:13 +0530

    Add: find-first-and-last-position-of-element-in-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-0.js
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    searchRange(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+        let result = [];
+
+        // First Binary Search --> Keep checking left if target found
+        while(l <= r) {
+            let mid = Math.floor((l + r) / 2);
+            console.log(mid)
+
+            if(nums[mid] === target) {
+                result[0] = mid;
+                r = mid - 1
+            }
+            else if(nums[mid] < target) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+
+        // Second Binary Search --> Keep checking right if target found 
+        l = 0;
+        r = nums.length - 1;
+
+        while(l <= r) {
+            let mid = Math.floor((l + r) / 2);
+
+            if(nums[mid] === target) {
+                result[1] = mid;
+                l = mid + 1;
+            }
+            else if(nums[mid] < target) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+
+        return result.length !== 0 ? result : [-1, -1];
+    }
+}
