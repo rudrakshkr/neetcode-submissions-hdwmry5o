189d2a9f53c9ffa414f65290f4f071b34c0d6eb0
Rudraksh <84639731+rudrakshkr@users.noreply.github.com>
2026-09-04 12:46:42 +0530

    Add: intersection-of-two-linked-lists - submission-4

---
+++ Data Structures & Algorithms/intersection-of-two-linked-lists/submission-4.js
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {ListNode} headA
+     * @param {ListNode} headB
+     * @return {ListNode}
+     */
+    getIntersectionNode(headA, headB) {
+        // Find the length of both the linked lists
+        let curr = headA;
+        let countA = 0;
+        let countB = 0;
+
+        while(curr !== null) {
+            countA += 1;
+            curr = curr.next;
+        }
+
+        curr = headB;
+
+        while(curr !== null) {
+            countB += 1;
+            curr = curr.next;
+        }
+
+        let currA = headA;
+        let currB = headB;
+
+        if(countA > countB) {
+            let diff = countA - countB;
+            while(diff > 0) {
+                currA = currA.next;
+                diff--;
+            }
+        }
+        else if(countA < countB) {
+            let diff = countB - countA;
+            while(diff > 0) {
+                currB = currB.next;
+                diff--;
+            }
+        }
+
+        while(currA !== null) {
+            if(currA === currB) return currA;
+            
+            currA = currA.next;
+            currB = currB.next;
+        }
+
+        return null; 
+    }
+}
